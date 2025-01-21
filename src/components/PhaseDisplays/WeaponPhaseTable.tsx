b05741e72fc756e1127bb88b4ffb17d402ede809
Will Mitchell <wmitchell@profoundlogic.com>
2025-01-21 17:29:09 -0500

    Improved table display

--- src/components/PhaseDisplays/WeaponPhaseTable.tsx
+++ src/components/PhaseDisplays/WeaponPhaseTable.tsx
@@ -9,43 +9,44 @@ function WeaponPhaseTable({
 }: {
   weaponDatasheets: DatasheetWargear[];
 }) {
-
   return (
-    <table className="table-auto bg-slate-800 mt-1 mb-0.5 pb-1 px-1 rounded-lg border-separate border-spacing-0 max-w-full overflow-auto">
-      <thead>
-        <tr>
-          <TableHeaderCell className="w-1/4">Name</TableHeaderCell>
-          <TableHeaderCell className="w-1/12">WS</TableHeaderCell>
-          <TableHeaderCell className="w-1/12">R"</TableHeaderCell>
-          <TableHeaderCell className="w-1/12">A</TableHeaderCell>
-          <TableHeaderCell className="w-1/12">S</TableHeaderCell>
-          <TableHeaderCell className="w-1/12">AP</TableHeaderCell>
-          <TableHeaderCell className="w-1/12">D</TableHeaderCell>
-          <TableHeaderCell className="w-1/4">KW</TableHeaderCell>
-        </tr>
-      </thead>
-      <tbody>
-        {weaponDatasheets.map((weapon, index) => (
-          <tr
-            key={index}
-            className={`${
-              index % 2 === 0 ? "bg-slate-600" : "bg-slate-700"
-            } text-sm`}
-          >
-            <TableCell className="w-1/4">{weapon.name}</TableCell>
-            <TableCell className="w-1/12">{weapon.BS_WS}+</TableCell>
-            <TableCell className="w-1/12">
-              {weapon.range === "Melee" ? weapon.range : `${weapon.range}"`}
-            </TableCell>
-            <TableCell className="w-1/12">{weapon.A}</TableCell>
-            <TableCell className="w-1/12">{weapon.S}</TableCell>
-            <TableCell className="w-1/12">{weapon.AP}</TableCell>
-            <TableCell className="w-1/12">{weapon.D}</TableCell>
-            <KeywordTags keywords={weapon.description?.split(", ")} />
+    <div className="overflow-x-auto">
+      <table className="table-auto bg-slate-800 mt-1 mb-0.5 pb-1 px-1 rounded-lg border-separate border-spacing-0 w-full ">
+        <thead>
+          <tr>
+            <TableHeaderCell className="w-1/4">Name</TableHeaderCell>
+            <TableHeaderCell className="w-1/12">WS</TableHeaderCell>
+            <TableHeaderCell className="w-1/12">R"</TableHeaderCell>
+            <TableHeaderCell className="w-1/12">A</TableHeaderCell>
+            <TableHeaderCell className="w-1/12">S</TableHeaderCell>
+            <TableHeaderCell className="w-1/12">AP</TableHeaderCell>
+            <TableHeaderCell className="w-1/12">D</TableHeaderCell>
+            <TableHeaderCell className="w-1/4">KW</TableHeaderCell>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {weaponDatasheets.map((weapon, index) => (
+            <tr
+              key={index}
+              className={`${
+                index % 2 === 0 ? "bg-slate-600" : "bg-slate-700"
+              } text-sm`}
+            >
+              <TableCell className="w-1/4">{weapon.name}</TableCell>
+              <TableCell className="w-1/12">{weapon.BS_WS}+</TableCell>
+              <TableCell className="w-1/12">
+                {weapon.range === "Melee" ? weapon.range : `${weapon.range}"`}
+              </TableCell>
+              <TableCell className="w-1/12">{weapon.A}</TableCell>
+              <TableCell className="w-1/12">{weapon.S}</TableCell>
+              <TableCell className="w-1/12">{weapon.AP}</TableCell>
+              <TableCell className="w-1/12">{weapon.D}</TableCell>
+              <KeywordTags keywords={weapon.description?.split(", ")} />
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
